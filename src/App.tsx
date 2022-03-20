9cc828457d56e6c054dc2920359f4b05e6809b78
KavishkaRajakaruna <rmkhrajakaruna@std.appsc.sab.ac.lk>
2022-03-20 09:30:10 +0530

    innitial message

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>OMIGOZ.COM</h1>
+      <h2>Convenience at your fingertips.</h2>
+      <img  src={process.env.PUBLIC_URL+'developer.jpg'} className='App-Developer'/>
     </div>
   );
 }
