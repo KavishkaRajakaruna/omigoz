62c3b8a08a211b020b912b1724294e6e7f49eb5c
KavishkaRajakaruna <rmkhrajakaruna@std.appsc.sab.ac.lk>
2022-03-20 09:45:54 +0530

    change title, logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>OMIGOZ.COM</h1>
+      <h1>OMiGOZ.COM</h1>
       <h2>Convenience at your fingertips.</h2>
       <img  src={process.env.PUBLIC_URL+'developer.jpg'} className='App-Developer'/>
     </div>
