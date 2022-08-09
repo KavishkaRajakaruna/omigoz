5c178de4f9066075e2dfecfb7e4ed36d4eed95d3
KavishkaRajakaruna <rmkhrajakaruna@std.appsc.sab.ac.lk>
2022-08-09 22:21:29 +0530

    telegram bot added

--- src/componants/home/Home.tsx
+++ src/componants/home/Home.tsx
@@ -2,10 +2,12 @@ import {motion} from "framer-motion";
 
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import Button from 'react-bootstrap/Button';
 
 
 import "./Home.css";
-import Countdown from "../Countdown/Countdown";
+// import Countdown from "../Countdown/Countdown";
+// import Feeder from "../feeder/Feeder";
 
 function Home(){
     return (
@@ -14,23 +16,28 @@ function Home(){
             <div className="Home-Landing">
                 
                 <div className="row">
-                <motion.div animate={{y: [-200,0], opacity:1}} transition={{ease: "easeIn", duration:1.5}} initial={{opacity:0}}>
+                <motion.div animate={{y: [-200,0], opacity:1}} transition={{ease: "easeIn", duration:1.2}} initial={{opacity:0}}>
                     <div className="flex-item">
                     <h1 className="display-1">OMiGOZ</h1>
                     </div>
                     <div className="flex-item">
                     <h2>Conevenience at your fingertips</h2>
-                    <a href="/mail_generator" className="btn btn-lg bg-light">Mail Alias Generator</a>
+                    <p> Following are the initial products. More are coming on the way. Enjoy !</p>
                     </div>
                     </motion.div>
                     &nbsp;
+
                     <div className="flex-item">
-                        <p className="lead font-italic">"Thank you for your patience. Our systems are currently under developement. We'll be online soon."</p>
+                    <motion.div initial={{opacity:0, scale: 0.4}} animate={{opacity:1, scale:1}} transition={{duration: 0.5, delay: 1.5}}>
+                    <Button variant="outline-light" href="/mail_generator" size="lg" style={{borderColor: "#0a9396"}}> Mail Alias Generator</Button> {'  '}
+                    
+                    <Button variant="outline-light" href="/feeder" size="lg" style={{borderColor: "#ffb703"}}> News Feed</Button> {''}
+                    <Button variant="outline-light" href="http://t.me/omigoz_bot" size="lg" style={{borderColor: "#e9d8a6"}}>Telegram Powercut Schedule Bot</Button>
+                    </motion.div>
                     </div>
+                    
                     &nbsp;
-                    <div className="flex-item">
-                    <h1 className="h2 text-muted"> <Countdown /></h1>
-                    </div>
+                    
                     
                 </div>               
                 
--- src/componants/maintainance/Maintanance.tsx
+++ src/componants/maintainance/Maintanance.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import './Maintain.css';
 
 function Maintanance(){
     return (
     <div className='App'>
-       <h1>OMiGOZ.COM</h1>
-      <h2>Convenience at your fingertips.</h2>
+       <h1>Sorry We're under maintainance </h1>
+      <h2>We will be back soon</h2>
+      {/* // eslint-disable-next-line jsx-a11y/alt-text */}
       <img  src={process.env.PUBLIC_URL+'developer.jpg'} className='App-Developer'/>
     </div>
     )
